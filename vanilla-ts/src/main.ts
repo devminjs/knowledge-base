95d040ae587ef01903876743280a9f8e754b90b6
devminjs <devminjs@protonmail.com>
2026-03-20 10:37:56 -0400

    chore: add tailwind

--- vanilla-ts/src/main.ts
+++ vanilla-ts/src/main.ts
@@ -1,7 +1,33 @@
-const [loadScribble] = Object.values(import.meta.glob("./scribble.ts"));
+import "./main.css";
 
-if (loadScribble) {
-  await loadScribble();
-} else {
-  console.log("create a scribble.ts and place it in src.");
+async function loadScript() {
+  const [loadScribble] = Object.values(import.meta.glob("./scribble.ts"));
+
+  if (loadScribble) {
+    await loadScribble();
+  } else {
+    console.log("create a scribble.ts and place it in src.");
+  }
 }
+
+function mountView() {
+  const app = document.getElementById("app");
+  if (app) {
+    app.innerHTML = `    
+    <div class="flex flex-col items-center justify-center h-screen">
+      <h1 class="text-2xl">Learn js coding questions!</h1>
+    </div>
+    `;
+  }
+}
+
+async function main() {
+  try {
+    mountView();
+    await loadScript();
+  } catch (error) {
+    console.error("Error loading script:", error);
+  }
+}
+
+await main();
--- vanilla-ts/vite.config.ts
+++ vanilla-ts/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite-plus";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  plugins: [tailwindcss()],
   lint: { options: { typeAware: true, typeCheck: true } },
 });
