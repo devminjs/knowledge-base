17ae4fe71dece580ad3c7728f63c8f40e92bf29e
devminjs <devminjs@protonmail.com>
2026-03-23 19:55:54 -0400

    chore: install tailwind

--- learn-vue/src/main.ts
+++ learn-vue/src/main.ts
@@ -1,8 +1,10 @@
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
 
-import App from './App.vue'
-import router from './router'
+import '~/styles/global.css'
+
+import App from '~/App.vue'
+import router from '~/router'
 
 const app = createApp(App)
 
